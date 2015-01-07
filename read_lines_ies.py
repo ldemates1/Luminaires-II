93fd5ece268a0dcf2d9e9fa6f471c9776329b5ef
Lauren DeMates <ren_003@hotmail.com>
2015-01-07 11:54:43 -0800

    Adding file name to string

--- read_lines_ies.py
+++ read_lines_ies.py
@@ -1,10 +1,8 @@
-#approach: open one file, read first line, and identify other data we want by taking only lines that start with [
-
 #reading one ies file
 my_file = open("/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaires/Undetermined/20352.ies")
 file_contents = my_file.read()
 
-#Create a template dictionary
+#Creating a template dictionary
 template_dict = {'IESNA':'','TEST':'','DATE':'','ISSUEDATE':'','MANUFAC':'','LUMCAT':'','LUMINAIRE':'','LAMP':'','BALLAST':'', 'DISTRIBUTION':'','OTHER':'', 'MORE':'',}
 
 #spliting file into lines
@@ -21,7 +19,23 @@ for line in lines:
         if header in template_dict:
             template_dict[header] = value
 
-#turn dictionary into string, but can't figure out how to get it in the order as template, \r is line break so good its in there right?
+#trying to get the first line recognized (only line we want without the [. Tried so many ways- I dont get why not pickup in the IESNA)
+    if ':' in line:
+        split_line_2 = line.split(':')
+        value_2 = split_line_2[1]
+        header_2 = split_line_2[0]
+        header_2 = header_2[1:]
+    if header_2 in template_dict:
+            template_dict[header] = value_2
+
+#trying to turn dictionary into string, but can't figure out how to get it in the order as template, the \r issue too
 raw_data_string=str(template_dict)
-data_1= my_file.name + raw_data_string
+
+#if we don't want headers -which is what discussed yesterday (doesn't work). I tried: for keys, values in template_dict(): print(values)
+
+#added file name to data 1 which will hopefully be string with all needed data (worked-yay)
+data_1= my_file.name +','+ raw_data_string
+
+#trying to get rid of \r (doesn't work)
+data_1=data_1.rstrip()
 print data_1
