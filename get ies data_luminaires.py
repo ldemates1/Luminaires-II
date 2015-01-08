deca59c4bf3b08e4462ec203ce9f71acce84a4f0
Lauren DeMates <ren_003@hotmail.com>
2015-01-08 14:26:00 -0800

    Building options to reiterate script through files
    Panda?

--- get ies data_luminaires.py
+++ get ies data_luminaires.py
@@ -1,21 +1,10 @@
-my_file = open("/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaires/Undetermined/*.ies")
-")
-file_contents = my_file.read()
-print(file_contents)
+#getting data from one ies file- grab script from read_lines_ies
+
+#run for different categories:
 
 
+#further clean up or analysis with pandas:
 
-#module to quickly write a loop over standard input or a list of files.
-my_file = open("/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaires/Undetermined/20352.ies")
-import fileinput
-for line in fileinput.input() :
-    fileinput.filename()
-
-
-
-
-
-#to read the filename, which i would like to save and include in final csv: fileinput.filename()
-
-#to close current file: fileinput.nextfile() Close the current file so that the next iteration will read the first line from /
-#the next file (if any); lines not read from the file will not count towards the cumulative line count."
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
