35e5b5b2a2edef677d6388c1d5a57310fa91f2dc
Lauren DeMates <ren_003@hotmail.com>
2015-01-09 17:03:09 -0800

    As a list-

--- read_lines_ies.py
+++ read_lines_ies.py
@@ -2,8 +2,12 @@
 my_file = open("/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaires/Undetermined/20352.ies")
 file_contents = my_file.read()
 
-#Creating a template dictionary, these are column headers on template
-template_dict = {'file_name':'','IESNA':'','TEST':'','DATE':'','ISSUEDATE':'','MANUFAC':'','LUMCAT':'','LUMINAIRE':'','LAMP':'','BALLAST':'', 'DISTRIBUTION':'','OTHER':'', 'MORE':'',}
+
+#Creating a list in the order of the column headers
+column_names = ['file_name','IESNA','TEST','DATE','ISSUEDATE','MANUFAC','LUMCAT','LUMINAIRE','LAMP','BALLAST','DISTRIBUTION','OTHER','MORE']
+
+#creating file_name variable to link to dictionary
+file_name = my_file.name
 
 #spliting file into lines
 lines = file_contents.split('\n')
@@ -15,37 +19,29 @@ for line in lines:
         value = split_line[1]
         header = split_line[0]
         header = header[1:]
-#filling in dictionary with values from ies files
-        if header in template_dict:
-            template_dict[header] = value
-
-#trying to get the first line recognized (only line we want without the [. Tried so many ways- I dont get why not pickup in the IESNA)
-    if ':' in line:
-        split_line_2 = line.split(':')
-        value_2 = split_line_2[1]
-        header_2 = split_line_2[0]
-        header_2 = header_2[1:]
-    if header_2 in template_dict:
-            template_dict[header] = value_2
-
-#creating file_name variable to link to dictionary
-file_name = my_file.name
-
-#transfering dictionairy to string
-raw_data_string=str(template_dict)
-
-#added file name to data 1 which will hopefully be string with all needed data (worked-yay), still not recognizing IESNA though!
-data_1= file_name +','+ raw_data_string
-
-print data_1
+if header in column_names:
+    column_names.append(value)
+    print column_names
 
 #accessing the csv I created with the dictionary keys, add wr?
 template_file = open("/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaires/Fields_template.csv")
 template_contents = template_file.read()
 
-#trying to say that if in dictionary then write data to file
-def data_2(data_1,template_contents)
-    if key in data_1 == template contents:
+#filling in dictionary with values from ies files
+        if header in column_names:
+            print value
+
+
+
+
+
+#added file name to data 1 which will hopefully be string with all needed data (worked-yay), still not recognizing IESNA though!
+data_1= file_name +','+ raw_data_string
+
+#accessing the csv I created with the dictionary keys, add wr?
+template_file = open("/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaires/Fields_template.csv")
+template_contents = template_file.read()
+
 
 
 #closing file, I think will be key to moving to next file
