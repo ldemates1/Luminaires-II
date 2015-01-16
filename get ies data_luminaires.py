61e76473dffcaa18d63f746a59f1247db3de7730
Lauren DeMates <ren_003@hotmail.com>
2015-01-16 15:23:47 -0800

    Trying to put not available in but not working

--- get ies data_luminaires.py
+++ get ies data_luminaires.py
@@ -8,11 +8,11 @@ mypath="/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaire
 isfile=os.path.isfile
 join=os.path.join
 number_of_files = sum(1 for item in os.listdir(mypath) if isfile(join(mypath, item)))
-print number_of_files
+
 
 #listing all the filenames in the directory
 list_of_filenames=listdir(mypath)
-print list_of_filenames
+
 
 for filename in list_of_filenames:
         my_file = open(str(mypath+'/'+filename))
@@ -39,17 +39,27 @@ for filename in list_of_filenames:
         ies_file.append(IESNA)
 
 #grabbing the lines we want, but I can't get it to recognize first line (IESA) though that is separated with :
+        headers = []
+        values= []
+
+        for line in lines:
+            if '[' in line:
+                split_line = line.split(']')
+                value = split_line[1]
+                header = split_line[0]
+                header = header[1:]
+                headers.append(header)
+                values.append(value)
+
+
         for column in column_names:
-
-            for line in lines:
-                if '[' in line:
-                    split_line = line.split(']')
-                    value = split_line[1]
-                    header = split_line[0]
-                    header = header[1:]
-
-                    if column == header:
-                        ies_file.append(value)
+            for i in range(0, len(headers)):
+                if column in headers:
+                    value = values[i]
+                    ies_file.append(value)
+                else:
+                    value = "Not available"
+                    ies_file.append(value)
 
             output_str = ''
             for i in range(len(ies_file)):
