d4c8126b901eacab7b4a83065b6863301f7069b5
Lauren DeMates <ren_003@hotmail.com>
2015-01-23 10:26:25 -0800

    Works!

--- headers_list.py
+++ headers_list.py
@@ -2,7 +2,7 @@ import os
 from os import listdir
 import csv
 
-mypath="/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaires/Commercial_Surface Mounted"
+mypath="/Users/DeMates/Google Drive/SFO Group Files/Projects/FEMP EEPP/Luminaires/Commercial_Suspended"
 
 output_file = '/Users/DeMates/Documents/Luminaires/Commercial_Suspended_List of Headers.csv'
 
@@ -18,28 +18,22 @@ for filename in list_of_filenames:
     for line in lines:
         # If a line starts with a bracket, then it might contain a relevant value
         if '[' in line:
-        # Split the line into header and value, and take out return characters
-        # Using rstrip to remove the return characters, and lstrip to remove
-        # preceeding whitespace from the value
+            # Split the line into header and value, and take out return characters
+            # Using rstrip to remove the return characters, and lstrip to remove
+            # preceeding whitespace from the value
             line_content = line.split(']')
 
-            line_header = line_content[0].split('[')
-            line_header = line_header[1].rstrip('[\r\n')
+            # If there are two elements
+            if(len(line_content) > 1):
+                line_header = line_content[0].split('[')
+    def unique(line_header):
+        uniques = []
+        for item in line_header:
+            if item not in uniques:
+                uniques.append(item)
+        return uniques
 
-        #turn string into a list to get uniquee values
-            all_headers_list= line_header.split()
-
-    #creating list and count of only unique headers
-    def unique_headers(all_headers_list):
-            keys = []
-            for e in all_headers_list:
-                if e not in keys:
-                    keys.append(e)
-
-                return keys
-                print keys
-                print len(keys)
-    # exporting the list of unqieu headers to csv
-            writer= csv.writer(open(output_file,'a'), delimiter=',')
-            writer.writerow([keys])
-            my_file.close()
+    # exporting the list of unique headers to csv
+        writer= csv.writer(open(output_file,'a'), delimiter=',')
+        writer.writerow([uniques])
+        my_file.close()
